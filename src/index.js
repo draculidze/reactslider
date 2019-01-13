cdfd484623eadfaf5bc8502cf15ee719fd061675
draculidze <draculidze@mail.ru>
2019-01-13 11:43:30 +0300

    Добавлены изображения слайдеров. Добавил вывод HelloWorld

--- src/index.js
+++ src/index.js
@@ -4,7 +4,32 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Hello() {
+	let phrase = "World";
+	return (
+			<h1>Привет, {phrase}!</h1>
+		)
+}
+
+function SayFullName(props) {
+	return (
+			<div>
+				<h1>Моё имя {props.name}, фамилия - {props.surname}</h1>
+				<a href={props.link}>Мой профиль</a>
+			</div>
+		)
+}
+
+function MeetAll() {
+	return (
+			<div>
+				<SayFullName />
+				<SayFullName name="Дмитрий" surname="Куреннов" link="vk.com"/>
+			</div>
+		)
+}
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
